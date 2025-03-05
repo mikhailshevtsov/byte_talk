7e33f68a84b37c49f7561f17ee358009d1fda551
mikhailshevtsov <mikhail.shevtsoff@gmail.com>
2025-03-05 09:02:53 +0000

    Add write buffers queue

--- src/connector.cpp
+++ src/connector.cpp
@@ -5,32 +5,39 @@ namespace bt
 
 bool connector::read_some()
 {
-    return m_read_buffer.read_some(get());
+    return io_some(m_read_buffer_size, m_read_bytes, m_read_buffer, ::read);
 }
 
 bool connector::write_some()
 {
-    return m_write_buffer.write_some(get());
+    return io_some(m_write_buffer_size, m_write_bytes, m_write_buffers_queue.front(), ::write);
 }
 
-void connector::load(std::vector<char>&& buffer)
+void connector::push(std::vector<char>&& buffer)
 {
-    m_write_buffer.load(std::move(buffer));
+    m_write_buffers_queue.push(std::move(buffer));
+    if (m_write_buffers_queue.size() > MAX_QUEUE_SIZE)
+        pop();
 }
 
-void connector::load(std::string_view buffer)
+void connector::push(std::string_view buffer)
 {
-    m_write_buffer.load(buffer);
+    push(std::vector<char>(std::cbegin(buffer), std::cend(buffer)));
+}
+
+void connector::pop()
+{
+    m_write_buffers_queue.pop();
 }
 
 std::string_view connector::read_buffer() const noexcept
 {
-    return m_read_buffer.get();
+    return std::string_view(m_read_buffer.data(), m_read_buffer.size());
 }
 
 std::string_view connector::write_buffer() const noexcept
 {
-    return m_write_buffer.get();
+    return std::string_view(m_write_buffers_queue.front().data(), m_write_buffers_queue.front().size());
 }
 
 }
--- src/connector.hpp
+++ src/connector.hpp
@@ -4,6 +4,7 @@
 #include "socket.hpp"
 #include "buffer.hpp"
 
+#include <queue>
 #include <vector>
 
 namespace bt
@@ -11,6 +12,10 @@ namespace bt
 
 class connector : public socket
 {
+private:
+    template <typename Func>
+    bool io_some(uint32_t& buffer_size, uint32_t& bytes, std::vector<char>& buffer, Func&& func);
+
 public:
     using socket::socket;
     using socket::operator=;
@@ -18,16 +23,63 @@ public:
     bool read_some();
     bool write_some();
 
-    void load(std::vector<char>&& buffer);
-    void load(std::string_view buffer);
+    void push(std::vector<char>&& buffer);
+    void push(std::string_view buffer);
+
+    void pop();
 
     std::string_view read_buffer() const noexcept;
     std::string_view write_buffer() const noexcept;
 
 private:
-    buffer m_read_buffer;
-    buffer m_write_buffer;
+    static constexpr uint32_t MAX_QUEUE_SIZE = 128;
+
+    std::vector<char> m_read_buffer;
+    uint32_t m_read_buffer_size = 0;
+    uint32_t m_read_bytes = 0;
+
+    std::queue<std::vector<char>> m_write_buffers_queue;
+    uint32_t m_write_buffer_size = 0;
+    uint32_t m_write_bytes = 0;
 };
+
+template <typename Func>
+bool connector::io_some(uint32_t& buffer_size, uint32_t& bytes, std::vector<char>& buffer, Func&& func)
+{
+    if (bytes < SIZE_BYTES)
+    {
+        int n = std::forward<Func>(func)(sockfd, reinterpret_cast<void*>(&buffer_size) + bytes, SIZE_BYTES - bytes);
+        if (n < 0)
+        {
+            perror("io(): buffer size");
+            exit(EXIT_FAILURE);
+        }
+        bytes += n;
+        if (bytes >= SIZE_BYTES)
+        {
+            buffer_size = ntohl(buffer_size);
+            buffer.resize(buffer_size);
+        }
+    }
+    else
+    {
+        uint32_t bytes_left = bytes - SIZE_BYTES; 
+        int n = std::forward<Func>(func)(sockfd, buffer.data() + bytes_left, buffer_size - bytes_left);
+        if (n < 0)
+        {
+            perror("io(): buffer");
+            exit(EXIT_FAILURE);
+        }
+        bytes += n;
+        bytes_left += n;
+        if (bytes >= buffer_size)
+        {
+            bytes = 0;
+            return true;
+        }
+    }
+    return false;
+}
 
 }
 
